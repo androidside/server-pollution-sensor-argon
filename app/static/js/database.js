ccaee8d79b8391e742583b7060177ee6681fae61
androidside2@Sauron <androidside2>
2020-04-17 13:47:08 -0400

    AJAX working

--- app/static/js/database.js
+++ app/static/js/database.js
@@ -4,8 +4,7 @@ function get_database(destElem) {
     $.post('/database').done(function(response) {
         if(response['text'] != '0'){
         	var readings = JSON.parse(response['text']);
-        	$(destElem).text('sucess!');
-//        	for loop and create HTML code to feed to destElem inner HTML
+        	show_database(destElem, readings);
         }
         else{
         	$(destElem).text('Database empty!');
@@ -13,4 +12,19 @@ function get_database(destElem) {
     }).fail(function() {
         $(destElem).text("{{ _('Error: Could not contact server.') }}");
     });
+}
+
+function show_database(destElem, readings){
+	var htmlString ="";
+
+	for (var i = 0; i < readings.length; i++) {
+		
+	htmlString +="<div><p><b>id</b>= "+readings[i].id+" <b>sensor_id</b>= "+readings[i].sensor_id+" <b>latitude</b>= "+readings[i].latitude+" <b>longitude</b>= "+readings[i].longitude+" <b>datetime</b>= "+readings[i].datetime+" <b>intensity</b>= "+readings[i].intensity+"</p></div>";	
+	}
+	
+	document.getElementById(destElem).innerHTML = htmlString;
+//	$(destElem).innerHTML(htmlString);
+//	for loop and create HTML code to feed to destElem inner HTML
+
+	
 }
